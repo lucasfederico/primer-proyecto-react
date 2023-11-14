12aacf1173ee92a9fd424af30926e80747781af9
Lucas <gasparlucas8@gmail.com>
2023-11-13 22:05:34 -0300

    first commit

---
+++ src/PrimerComponente.jsx
@@ -0,0 +1,9 @@
+let variable = 'hola'
+export const PrimerComponente = () => {
+ 
+  return (
+    <>
+    <h1>{variable}3</h1>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {PrimerComponente} from './PrimerComponente'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrimerComponente/>
+  </React.StrictMode>,
+)
